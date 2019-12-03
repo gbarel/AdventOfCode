3801971851605b3184db1cae51dfc892a9362e65
gbarel <gaetan.barel@gmail.com>
2019-12-02 22:18:46 -0500

    December 2 2019

--- 2019/2019-12-02.r
+++ 2019/2019-12-02.r
@@ -111,7 +111,7 @@ get-code: func [
     if (not none? ptr) and (index < length? program) [
         index: (pick program index) + 1
     ]
-    print rejoin ["get-code [" :program "] " :pos " -> idx=" :index]
+    ; print rejoin ["get-code [" :program "] " :pos " -> idx=" :index]
     pick program index
 ]
 
@@ -122,7 +122,7 @@ set-code: func [
 ] [
     ; remap index from [0-N] to [1-N]
     index: pos + 1
-    print rejoin ["set-code [" :program "] " :pos " " :val " -> idx=" :index]
+    ; print rejoin ["set-code [" :program "] " :pos " " :val " -> idx=" :index]
     change/part (at program index) val 1
     program
 ]
@@ -136,7 +136,7 @@ step-code: func [
     w1: get-code/ptr program (pos + 1)
     w2: get-code/ptr program (pos + 2)
     w3: get-code program (pos + 3)
-    probe rejoin [op " " w1 " " w2 " "w3]
+    ; probe rejoin [op " " w1 " " w2 " "w3]
     switch op [
         1 [ set-code program w3 (w1 + w2) ]
         2 [ set-code program w3 (w1 * w2) ]
@@ -150,25 +150,89 @@ run-code: func [
 ] [
     pos: 0
     until [
-        print program
         op: step-code program pos
         pos: pos + 4
         op = 99
     ]
-    print program
     get-code program 0
 ]
 
-map: func [block 'fn] [
-    result: make block! length? block
-    foreach elem block [
-        append result (do fn elem)
-    ]
-    result
-]
+; 1. parse input
+input-file: %2019-12-02-input.txt
+; data: load replace/all read filepath "," " "
+data: map-each entry parse read input-file "," [to-integer entry]
 
-parse-input: func [
-    filepath [path!] "the input file"
-] [
-    map (parse (read filepath) ",") to-integer
+; 1.1 correct input values
+data-part1: copy data
+set-code data-part1 1 12 ; nound=12
+set-code data-part1 2 2  ; verb=2
+
+; 2. run code on loaded data
+prin "Part 1: " print run-code data-part1
+
+comment {
+    --- Part Two ---
+
+    "Good, the new computer seems to be working correctly! Keep it nearby during
+    this mission - you'll probably use it again. Real Intcode computers support 
+    many more features than your new one, but we'll let you know what they are 
+    as you need them."
+
+    "However, your current priority should be to complete your gravity assist 
+    around the Moon. For this mission to succeed, we should settle on some 
+    terminology for the parts you've already built."
+
+    Intcode programs are given as a list of integers; these values are used as 
+    the initial state for the computer's memory. When you run an Intcode 
+    program, make sure to start by initializing memory to the program's values. 
+    A position in memory is called an address (for example, the first value in 
+    memory is at "address 0").
+
+    Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values 
+    used immediately after an opcode, if any, are called the instruction's 
+    parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, 
+    and 4 are the parameters. The instruction 99 contains only an opcode and has
+    no parameters.
+
+    The address of the current instruction is called the instruction pointer; 
+    it starts at 0. After an instruction finishes, the instruction pointer 
+    increases by the number of values in the instruction; until you add more 
+    instructions to the computer, this is always 4 (1 opcode + 3 parameters) for
+    the add and multiply instructions. (The halt instruction would increase the
+    instruction pointer by 1, but it halts the program instead.)
+
+    "With terminology out of the way, we're ready to proceed. To complete the 
+    gravity assist, you need to determine what pair of inputs produces the 
+    output 19690720."
+
+    The inputs should still be provided to the program by replacing the values 
+    at addresses 1 and 2, just like before. In this program, the value placed in
+    address 1 is called the noun, and the value placed in address 2 is called 
+    the verb. Each of the two input values will be between 0 and 99, inclusive.
+
+    Once the program has halted, its output is available at address 0, also just
+    like before. Each time you try a pair of inputs, make sure you first reset 
+    the computer's memory to the values in the program (your puzzle input) - in 
+    other words, don't reuse memory from a previous attempt.
+
+    Find the input noun and verb that cause the program to produce the output 
+    19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, 
+    the answer would be 1202.)
+}
+
+use [inputs] [
+    inputs: []
+    for noun 0 99 1 [
+        for verb 0 99 1 [
+            data-temp: copy data
+            set-code data-temp 1 noun
+            set-code data-temp 2 verb
+            output: run-code data-temp            
+            if (output = 19690720) [
+                append inputs ( noun * 100 + verb )
+            ]
+        ]
+    ]
+    prin "Answer part 2: "
+    print inputs
 ]
