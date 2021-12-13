e384123b6b21db5a59139edc08f5d56546169285
gbarel <gaetan.barel@gmail.com>
2021-12-13 01:30:04 -0500

    beautify 2021-12-13

--- 2021/13.r
+++ 2021/13.r
@@ -6,61 +6,62 @@ REBOL [
 ; import utility functions
 do %util.r
 
-data: split read/lines %data/13.txt ""
-points: []  foreach point data/1 [
-  parse point [
-    copy x to "," skip copy y to end
-    (append points as-pair to-integer x to-integer y)
-  ]
-]
 
-fold: funct [
-  points [block!]
-  line [pair!]
-][
-  either line/1 <> 0 [
-    ; fold horizontally
-    forall points [all [
-        points/1/x > line/1
-        points/1: as-pair (2 * line/1 - points/1/x) points/1/y
-    ]]
-  ][
-    ; fold vertically
-    forall points [all [
-      points/1/y > line/y
-      points/1: as-pair points/1/x  (2 * line/2 - points/1/y)
-    ]]
-  ]
-  points: unique points
-]
+points: context [
+  process: draw: dots: inst: none 
+  use [data fold data] [
+    data: split read/lines %data/13.txt ""
+    ; parse data into dots and inst
+    dots: map-each line data/1 [load replace line {,} {x}]
+    inst: data/2
 
-draw-points: funct [
-  points [block!]
-][
-  dims: 0x0  foreach p points [dims: max dims p]
-  dots: array/initial reduce [dims/y + 1 dims/x + 1] "."
-  foreach p points [dots/(p/y + 1)/(p/x + 1): "#"]
-  foreach line dots [
-    print reform line
+    draw: funct [][
+      m: 0x0  foreach p dots [m: max m p]
+      res: array/initial reduce [m/y + 1 m/x + 1] "."
+      foreach p dots [res/(p/y + 1)/(p/x + 1): "#"]
+      foreach line res [print rejoin line]
+    ]
+
+    fold: funct [
+      line [pair!]
+    ][
+      either line/1 <> 0 [
+        ; fold horizontally
+        forall dots [all [
+          dots/1/x > line/1
+          dots/1: as-pair (2 * line/1 - dots/1/x) dots/1/y
+        ]]
+      ][
+        ; fold vertically
+        forall dots [all [
+          dots/1/y > line/y
+          dots/1: as-pair dots/1/x  (2 * line/2 - dots/1/y)
+        ]]
+      ]
+      set 'dots unique dots
+    ]
+
+    process: func [
+      /all
+    ][
+      loop either all [length? inst][1][
+        x: y: 0 parse inst/1 ["fold along " 
+          ["x=" copy x to end | "y=" copy y to end] 
+        ]
+        fold as-pair to-integer x to-integer y
+        inst: next inst
+      ]
+    ]
   ]
 ]
 
 ; --- Part 1 ---
-x: y: 0 parse data/2/1 ["fold along " 
-  ["x=" copy x to end | "y=" copy y to end] 
-]
-fold points as-pair to-integer x to-integer y
-
-r1: length? unique points
+points/process
+r1: length? points/dots
 print [{Part 1:} r1]
 
 ; --- Part 2 ---
-for i 2 length? data/2 1 [
-  x: y: 0 parse data/2/:i ["fold along " 
-    ["x=" copy x to end | "y=" copy y to end] 
-  ]
-  fold points as-pair to-integer x to-integer y
-]
-draw-points points
+points/process/all
+points/draw
 r2: 0
 print [{Part 2:} r2]
